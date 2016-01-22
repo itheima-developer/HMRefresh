ce6be3f93670b75dd9d25f1c8283d55bfe981e43
刀哥 <liufan321@gmail.com>
2016-01-22 09:57:42 +0800

    KVO 监听，修改 UIRefreshControl 的事件发送方式

--- HMRefresh/HMRefresh/HMRefresh/HMRefreshControl.m
+++ HMRefresh/HMRefresh/HMRefresh/HMRefreshControl.m
@@ -8,6 +8,58 @@
 
 #import "HMRefreshControl.h"
 
-@implementation HMRefreshControl
+@implementation HMRefreshControl {
+    __weak id __target;
+    SEL __action;
+    
+    BOOL _isRefreshing;
+}
+
+#pragma mark - 构造函数
+- (instancetype)init {
+    self = [super init];
+    if (self) {
+        dispatch_async(dispatch_get_main_queue(), ^{
+            [self addObserver:self forKeyPath:@"frame" options:0 context:nil];
+        });
+    }
+    return self;
+}
+
+- (void)dealloc {
+    [self removeObserver:self forKeyPath:@"frame"];
+    NSLog(@"%s", __FUNCTION__);
+}
+
+#pragma mark - 父类方法及属性
+- (BOOL)isRefreshing {
+    return _isRefreshing || _isPullupRefresh;
+}
+
+- (void)addTarget:(id)target action:(SEL)action forControlEvents:(UIControlEvents)controlEvents {
+    __target = target;
+    __action = action;
+}
+
+- (void)beginRefreshing {
+    NSLog(@"开始刷新");
+    [super beginRefreshing];
+}
+
+- (void)endRefreshing {
+    NSLog(@"结束刷新");
+    [super endRefreshing];
+    
+    _isRefreshing = NO;
+}
+
+#pragma mark - KVO 相关方法
+- (void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary<NSString *,id> *)change context:(void *)context {
+    // 发送刷新事件
+    if (self.frame.origin.y < -80 && _isRefreshing == NO) {
+        _isRefreshing = YES;
+        [self sendAction:__action to:__target forEvent:[[UIEvent alloc] init]];
+    }
+}
 
 @end
